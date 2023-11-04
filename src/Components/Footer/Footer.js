85e24b881c3f5e214d12b9d6f206649b309cdd6e
Viktor Kitanovski <viktor.kitanovski45@gmail.com>
2023-11-04 21:53:01 +0100

    Add Footer component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="container pt-5 pb-5 text-center">
+                <div className="row">
+                    <p>© 2023 by <p className="light-blue d-inline-block fw-bold">Viktor Kitanovski</p></p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
