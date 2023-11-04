6f7a3febbdf0bb6db5ff6b9b30dac05c884ed3f9
Viktor Kitanovski <viktor.kitanovski45@gmail.com>
2023-11-04 22:46:23 +0100

    Add Credentials Page

---
+++ src/Pages/Credentials/CredentialsPage.js
@@ -0,0 +1,15 @@
+import Navigation from "../../Components/Navigation/Navigation";
+import Footer from "../../Components/Footer/Footer";
+import CredentialsLayout from "../../Layouts/Credentials Layout/CredentialsLayout";
+
+const CredentialsPage = () => {
+    return (
+        <>
+            <Navigation/>
+            <CredentialsLayout/>
+            <Footer/>
+        </>
+    )
+}
+
+export default CredentialsPage;
