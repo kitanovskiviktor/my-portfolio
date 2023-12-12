cce83bdf658bef15af43cd5adaf223efd1b0da39
Viktor Kitanovski <74512777+kitanovskiviktor@users.noreply.github.com>
2023-12-12 23:51:49 +0100

    Add Send Message functionality

--- src/Utils/appFunctions/appFunctions.js
+++ src/Utils/appFunctions/appFunctions.js
@@ -1,25 +1,25 @@
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 export const sendEmail = (name, email, subject, message) => {
-
-    const emailHTML = `
+  const emailHTML = `
        <div>
-       <h3>Name: ${name.current.value}</h3>
-       <h3>Email: ${email.current.value}</h3>
-       <h3>Subject: ${subject.current.value}</h3>
-       <h3>Message: ${message.current.value}</h3>
+       <h3>Name: ${name}</h3>
+       <h3>Email: ${email}</h3>
+       <h3>Subject: ${subject}</h3>
+       <h3>Message: ${message}</h3>
        </div>
     `;
 
-
-    emailjs.sendForm('service_36e1ri3', 'template_7vg3lrf', {
-        html_content: emailHTML,
-        html: true,
-    }, '5kRqf-XJZCU7-czxr')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-
-}
+  emailjs.init("5kRqf-XJZCU7-czxr");
+  emailjs
+    .send("service_36e1ri3", "template_7vg3lrf", {
+      html_content: emailHTML,
+      html: true,
+    })
+    .then((response) => {
+      console.log("Email sent:", response);
+    })
+    .catch((error) => {
+      console.error("Email could not be sent:", error);
+    });
+};
