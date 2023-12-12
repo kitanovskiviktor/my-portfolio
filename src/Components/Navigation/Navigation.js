f26fec594e8b3d9e6477a4fcecb03c572664328e
Viktor Kitanovski <74512777+kitanovskiviktor@users.noreply.github.com>
2023-12-12 23:50:11 +0100

    Change Navigation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,16 +8,16 @@ const Navigation = () => {
     return (
         <>
       <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container justify-content-around">
-                <Link to="/" className="navbar-brand"><img src={logo} alt='' className="nav-logo"/></Link>
+                <Link to="/"><img src={logo} alt='' className="nav-logo"/></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav justify-content-between align-items-center w-100">
                     <div className='nav-items-list'>
-                        <Link to="/" className='active'>Home</Link>
+                        <Link to="/">Home</Link>
                         <Link to="/about">About</Link>
                         <Link to="/works">Works</Link>
                         <Link to="/contact">Contact</Link>
